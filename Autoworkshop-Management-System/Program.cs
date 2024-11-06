a389606a9aeaa3a60b9ed823e056c0c5f0d72768
Tobias Hougaard <Tobiashougaard13@outlook.com>
2024-11-06 13:24:46 +0100

    Add project files.

---
+++ Autoworkshop-Management-System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Autoworkshop_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

77917be7b2e9dbe6623bcb0c576eb2ec53b8d8cb
Mikkel Schmidt <mikkelschmidt@msn.com>
2024-11-06 13:48:53 +0100

    Tilføjet Vehicle class med properties

---
+++ Autoworkshop-Management-System/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autoworkshop_Management_System
+{
+    internal class Vehicle
+    {
+        public int VechicleID { get; set; }
+
+        public string Model { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Year { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public int Wheels { get; set; }
+
+        public string GearType { get; set; }
+    }
+}

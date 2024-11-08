06b8a43c870ebb2564560fd385f3e040687faa49
Mikkel Schmidt <mikkelschmidt@msn.com>
2024-11-08 10:14:36 +0100

    Tilføjet reader til Motorcycle.cs

--- Autoworkshop-Management-System/Motorcycle.cs
+++ Autoworkshop-Management-System/Motorcycle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,51 @@ namespace Autoworkshop_Management_System
 
         public override void GetVechicleData()
         {
-            Console.WriteLine($"{VechicleID} {Model} {Handles}");
+            List<Motorcycle> ListOfCars = new List<Motorcycle>();
+
+            string ConnectionString = "Data Source=localhost;" +
+                                      "Initial Catalog=Autoworkshop;" +
+                                      "Integrated Security=SSPI;" +
+                                      "TrustServerCertificate=true";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Motorcycle";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Motorcycle vehicleFromDB = new Motorcycle();
+                            vehicleFromDB.VechicleID = Convert.ToInt32(reader["VehicleID"]);
+                            vehicleFromDB.Model = reader["Model"].ToString();
+                            vehicleFromDB.Brand = reader["Brand"].ToString();
+                            vehicleFromDB.Year = Convert.ToInt32(reader["Year"]);
+                            vehicleFromDB.LicensePlate = reader["LicensePlate"].ToString();
+                            vehicleFromDB.Wheels = Convert.ToInt32(reader["Wheels"]);
+                            vehicleFromDB.GearType = reader["GearType"].ToString();
+                            vehicleFromDB.Handles = Convert.ToInt32(reader["Handles"]);
+
+                            Console.WriteLine($"" +
+                                $"------------------------------\n" +
+                                $"ID: {vehicleFromDB.VechicleID}\n" +
+                                $"BRAND: {vehicleFromDB.Brand}\n" +
+                                $"MODEL: {vehicleFromDB.Model}\n" +
+                                $"YEAR: {vehicleFromDB.Year}\n" +
+                                $"LICENSEPLATE: {vehicleFromDB.LicensePlate}\n" +
+                                $"WHEELS: {vehicleFromDB.Wheels}\n" +
+                                $"GEARTYPE: {vehicleFromDB.GearType}\n" +
+                                $"HANDLES: {vehicleFromDB.Handles}\n" +
+                                $"------------------------------\n");
+                        }
+                    }
+                }
+            }
         }
+    }
     }
 }

ec368a182b757a71091d59c78781abf8a40d4a2e
Mikkel Schmidt <mikkelschmidt@msn.com>
2024-11-08 10:11:02 +0100

    Flyttet reader af car data fra program.cs til car.cs

--- Autoworkshop-Management-System/Car.cs
+++ Autoworkshop-Management-System/Car.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,52 @@ namespace Autoworkshop_Management_System
 
         public override void GetVechicleData()
         {
-            Console.WriteLine($"{VechicleID} {Model} {Doors} {Airbags}");
+            List<Car> ListOfCars = new List<Car>();
+
+            string ConnectionString = "Data Source=localhost;" +
+                                      "Initial Catalog=Autoworkshop;" +
+                                      "Integrated Security=SSPI;" +
+                                      "TrustServerCertificate=true";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Car";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Car vehicleFromDB = new Car();
+                            vehicleFromDB.VechicleID = Convert.ToInt32(reader["VehicleID"]);
+                            vehicleFromDB.Model = reader["Model"].ToString();
+                            vehicleFromDB.Brand = reader["Brand"].ToString();
+                            vehicleFromDB.Year = Convert.ToInt32(reader["Year"]);
+                            vehicleFromDB.LicensePlate = reader["LicensePlate"].ToString();
+                            vehicleFromDB.Wheels = Convert.ToInt32(reader["Wheels"]);
+                            vehicleFromDB.GearType = reader["GearType"].ToString();
+                            vehicleFromDB.Doors = Convert.ToInt32(reader["Doors"]);
+                            vehicleFromDB.Airbags = Convert.ToInt32(reader["Airbags"]);
+
+                            Console.WriteLine($"" +
+                                $"------------------------------\n" +
+                                $"ID: {vehicleFromDB.VechicleID}\n" +
+                                $"BRAND: {vehicleFromDB.Brand}\n" +
+                                $"MODEL: {vehicleFromDB.Model}\n" +
+                                $"YEAR: {vehicleFromDB.Year}\n" +
+                                $"LICENSEPLATE: {vehicleFromDB.LicensePlate}\n" +
+                                $"WHEELS: {vehicleFromDB.Wheels}\n" +
+                                $"GEARTYPE: {vehicleFromDB.GearType}\n" +
+                                $"DOORS: {vehicleFromDB.Doors}\n" +
+                                $"AIRBAGS: {vehicleFromDB.Airbags}\n" +
+                                $"------------------------------\n");
+                        }
+                    }
+                }
+            }
         }
     }
 }
